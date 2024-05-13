4c5bbf0c608a2fc23294a81543c8fae7dfd7bf49
marvic soliman <marvi>
2024-05-13 17:23:01 +0800

    changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is for module 1 and 2 for aws '));
+app.get('/', (req, res) => res.send('Testing'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
