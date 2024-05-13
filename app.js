17ca64da2a44eae8b40cf3b0253c552e436e6344
marvic soliman <marvi>
2024-05-13 15:46:53 +0800

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is for lab_Marvic2222'));
+app.get('/', (req, res) => res.send('This is for Labwork4_Marvic'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
